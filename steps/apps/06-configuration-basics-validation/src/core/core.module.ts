f6ce221629504deadfc3dd2f4c226ca165903fa9
Beatriz Rochelle <rochelle.beatriz@gmail.com>
2023-02-27 15:48:47 +0100

    chore: 06-configuration-basics-validation
    validate port number with Joi

--- steps/apps/06-configuration-basics-validation/src/core/core.module.ts
+++ steps/apps/06-configuration-basics-validation/src/core/core.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { resolve } from 'path';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: resolve(__dirname, 'environments', 'local.env'),
+      validationSchema: Joi.object({
+        PORT: Joi.number().required()
+      })
     }),
   ],
   exports: [ConfigModule],
